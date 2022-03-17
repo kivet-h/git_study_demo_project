15ad003346126587e171ef4f5e457cb8c5c400d4
kivet <wanghuixiang@druid.tech>
2022-03-17 15:27:04 +0800

    c

--- demo1.js
+++ demo1.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: kivet
  * @Date: 2021-11-22 17:10:45
- * @LastEditTime: 2022-03-17 10:31:51
+ * @LastEditTime: 2022-03-17 15:26:45
  */
 
 const a = 'aaa';
@@ -10,3 +10,6 @@ console.log(a);
 
 const b = 'bbb';
 console.log(b);
+
+const c = 'ccc';
+console.log(c);
