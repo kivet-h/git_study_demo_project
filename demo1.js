1d35b03a88f3e14dcb80734dc2729c5b8d9ce0ba
kivet <wanghuixiang@druid.tech>
2022-03-18 13:50:07 +0800

    cccc

--- demo1.js
+++ demo1.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: kivet
  * @Date: 2021-11-22 17:10:45
- * @LastEditTime: 2022-03-17 16:31:21
+ * @LastEditTime: 2022-03-18 13:49:58
  */
 
 const a = 'aaaa';
@@ -11,5 +11,5 @@ console.log(a);
 const b = 'bbb';
 console.log(b);
 
-const c = 'cc';
+const c = 'cccc';
 console.log(c);
